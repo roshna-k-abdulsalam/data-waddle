d2d9808f24d460c4e0cb7c48f57c4b7b82b57b43
roshna-k-abdulsalam <roshna@hamon.in>
2023-06-18 18:20:49 +0530

    Adds the commands that perform the subtasks from downloading csv files to batch transfer data to cloud storage

--- travel.sh
+++ travel.sh
@@ -1,3 +1,4 @@
-PGPASSWORD=<user-postgresql-password> psql -h <ip/url-of-the-remote-server> -U <user-name> -d <database-name> </path/to/your/local/file.sql
-python data_clean_csv.py
-
+python download_dataset.py
+PGPASSWORD=<user-postgresql-password> psql -h <ip/url-of-the-remote-server> -U <user-name> -d <database-name> -w -f travel.sql
+python transform_load_csv_data.py
+python batch_transfer_data_to_cloud_storage.py
